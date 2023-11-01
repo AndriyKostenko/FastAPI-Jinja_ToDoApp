37567dcd6f70e4673e0c4fbab063042a8b03e530
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-11-01 18:44:38 -0400

    func. for add. new todo compl.

--- routers/todos.py
+++ routers/todos.py
@@ -2,8 +2,11 @@ import sys
 
 sys.path.append("..")
 
+from starlette.responses import RedirectResponse
+from starlette import status
+
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi import Depends, HTTPException, APIRouter, Request, Form
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -33,8 +36,11 @@ def get_db():
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+    
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
@@ -42,6 +48,27 @@ async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, 
+                      title: str = Form(...), 
+                      description: str = Form(...),
+                      priority: int = Form(...), 
+                      db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
 async def edit_todo(request: Request):
     return templates.TemplateResponse("edit-todo.html", {"request": request})
+
+
